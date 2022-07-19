d9930ae8d58e6fe9bbd045ce4c897df1346ae6dc
homopoluza <homopoluza@gmail.com>
2022-07-19 18:20:07 +0300

    divide and complete terraform

--- final/terraform/main.tf
+++ final/terraform/main.tf
@@ -5,14 +5,14 @@ terraform {
     }
   }
   required_version = ">= 0.13"
-    backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "final-bucket"
-    region     = "ru-central1"
-    key        = ".terraform/terraform.tfstate"
+  backend "s3" {
+    endpoint                    = "storage.yandexcloud.net"
+    bucket                      = "final-bucket"
+    region                      = "ru-central1"
+    key                         = ".terraform/terraform.tfstate"
     skip_region_validation      = true
     skip_credentials_validation = true
-    }
+  }
 }
 
 variable "OAuth" {
@@ -29,69 +29,18 @@ provider "yandex" {
 locals {
   instance_count = {
     stage = 1
-    prod = 1
-  }
-  instance_core = {
-    stage = 4
-    prod = 4
-  }
-  instance_memory = {
-    stage = 4
-    prod = 4
+    prod  = 1
   }
   vpc_zone = {
     stage = "ru-central1-a"
-    prod = "ru-central1-b"
+    prod  = "ru-central1-b"
   }
   vpc_v4_cidr_blocks = {
     stage = ["192.168.10.0/24"]
-    prod = ["192.168.20.0/24"]
+    prod  = ["192.168.20.0/24"]
   }
   workspace = {
     stage = "stage"
-    prod = "prod"
+    prod  = "prod"
   }
-}
-
-resource "yandex_compute_instance" "vm-1" {
-  count = "${local.instance_count[terraform.workspace]}"
-  name = "final-${count.index}-${local.workspace[terraform.workspace]}"
-  resources {
-    cores  = local.instance_core[terraform.workspace]
-    memory = local.instance_memory[terraform.workspace]
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd81u2vhv3mc49l1ccbb"
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/yc_rsa.pub")}"
-  }
-}
-
-resource "yandex_vpc_network" "network-1" {
-  name = "final-network-${local.workspace[terraform.workspace]}-${local.instance_count[terraform.workspace]}"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet-${local.instance_count[terraform.workspace]}-${local.workspace[terraform.workspace]}"
-  zone           = local.vpc_zone[terraform.workspace]
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = local.vpc_v4_cidr_blocks[terraform.workspace]
-}
-
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.*.network_interface.0.ip_address
-}
-
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.*.network_interface.0.nat_ip_address
 }
---
+++ final/terraform/nginx.tf
@@ -0,0 +1,24 @@
+resource "yandex_compute_instance" "rev-proxy" {
+  name  = "rev-proxy-${local.workspace[terraform.workspace]}"
+  hostname = "homopoluza.ru"
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd81u2vhv3mc49l1ccbb"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+    ip_address = "192.168.10.10"
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/yc_rsa.pub")}"
+  }
+}
