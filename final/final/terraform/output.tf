ff5050488e55f8ea3265594bb52bc83c0b1eb57d
homopoluza <83767598+homopoluza@users.noreply.github.com>
2023-01-31 04:03:59 +0600

    Delete final/final directory

--- final/final/terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "internal_ip_address_rev-proxy" {
-  value = yandex_compute_instance.rev-proxy.*.network_interface.0.ip_address
-}
-
-output "external_ip_address_rev-proxy" {
-  value = yandex_compute_instance.rev-proxy.*.network_interface.0.nat_ip_address
-}
